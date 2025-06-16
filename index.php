2563888170fc9a3899d92cc6dd04a664b96922ad
Rwabutaguza-ASLAM <rwabu.aslam30@gmail.com>
2025-06-17 00:11:28 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Rwabutaguza Aslam - Request/Response Cycle</title>
+</head>
+<body>
+  <h1>Rwabutaguza Aslam Request / Response</h1>
+  <p>The SHA256 hash of "Rwabutaguza Aslam" is</p>
+<pre>
+ASCII ART:
+
+   *****    
+*    *   
+*    *   
+*****    
+*  *     
+*   *    
+*    *
+
+</pre>
+
+   <?php
+    echo hash('sha256', 'Rwabutaguza Aslam');
+    ?>
+   <a href="check.php">Click here to check the error setting</a>
+  <br/>
+  <a href="fail.php">Click here to cause a traceback</a>
+</body>
+</html>
